5ecf4e600a5d5821646d579e9059a5a030a4c25e
Christina <yickind@hotmail.co.uk>
2024-10-14 22:50:44 +0100

    Updated App.js with tab navigation, context provider and background

--- App.js
+++ App.js
@@ -1,24 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, StyleSheet } from "react-native";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import HomeScreen from './screens/HomeScreen';
-import ForecastScreen from './screens/ForecastScreen';
+import HomeScreen from "./screens/HomeScreen";
+import ForecastScreen from "./screens/ForecastScreen";
+import LocationContextProvider from "./context/location-context";
 
-const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style='light' />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Forecast" component={ForecastScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <StatusBar style="light" />
+      <LocationContextProvider>
+        <ImageBackground
+          source={require("./assets/sky_background.jpg")}
+          resizeMode="cover"
+          style={styles.background}
+        >
+          <NavigationContainer>
+            <BottomTab.Navigator
+              sceneContainerStyle={{ backgroundColor: "transparent" }}
+              screenOptions={{
+                headerStyle: { backgroundColor: "transparent" },
+                headerTintColor: "white",
+                tabBarActiveTintColor: "#528ab4",
+              }}
+            >
+              <BottomTab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="home" color={color} size={size} />
+                  ),
+                }}
+              />
+              <BottomTab.Screen
+                name="Forecast"
+                component={ForecastScreen}
+                options={{
+                  headerTransparent: true,
+                  tabBarIcon: ({ color, size }) => (
+                    <Ionicons name="partly-sunny" color={color} size={size} />
+                  ),
+                }}
+              />
+            </BottomTab.Navigator>
+          </NavigationContainer>
+        </ImageBackground>
+      </LocationContextProvider>
     </>
   );
 }
@@ -26,8 +60,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  background: {
+    width: "100%",
+    height: "100%",
   },
 });
